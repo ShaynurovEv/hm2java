1c608f73d472b2be19490e4adb582a8e72ddd940
Zhenya <shaynurov75@gmail.com>
2023-05-06 19:34:05 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int k = 20;
+        int bonus = ticketPrice / k;
+        System.out.println("Количество бонусных миль = " + bonus);
+    }
+}
